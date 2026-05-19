c33fe441c72924eecf02734279028c7aaa701167
Regina-Cerón189 <rceronf2400@alumno.ipn.mx>
2026-05-19 17:12:19 -0600

    Modificación del gitignore

--- ExamenSegundoParcial/Server/server.js
+++ ExamenSegundoParcial/Server/server.js
@@ -1,28 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'n0m3l0',
-    database: 'pnt_practica1',
+
+// Al usar mysql2/promise, este pool ya genera promesas por sí solo
+const db = mysql.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'GA7reg@108',
+    database: process.env.DB_NAME || 'pnt_practica1',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
-});
-const db = pool.promise();
-app.use(express.json());
-app.use(express.static(path.join(__dirname, '..', 'Public'))); 
-app.use((req, res, next) => {
-    console.log(`[${new Date().toLocaleTimeString()}] ${req.method} ${req.url}`);
-    next();
-});
-app.get('/api/test', (req, res) => {
-    res.json({ mensaje: "Conectado al backend con éxito" });
-});
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto: ${PORT}`);
-    console.log('Para salir presiona ctrl + c');
 });
